24192d210c4eff3ded8ceee8e5e409e44e5c7c31
S-L-9999 <hongsovanleonan@gmail.com>
2024-11-19 09:44:14 +0700

    Change name and added git ignore.

--- Flowchart Pseudocode/timesTen.py
+++ Flowchart Pseudocode/timesTen.py
@@ -1,8 +1,8 @@
-def timesten(x):
+def timesTen(x):
     return x * 10
 
 try:
     inputValue = float(input("What number would you like to multiply by 10? \n"))
-    print("The result is", round(timesten(inputValue),3))
+    print("The result is", round(timesTen(inputValue),3))
 except ValueError:
     print("Error: This is invalid please enter a valid number.")
